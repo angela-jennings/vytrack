219512671e6b352b59733e18a47cf304f89b8a4e
angela-jennings <amjennings118@gmail.com>
2021-06-05 15:07:07 -0600

    Test case 1 - I forgot to put on a separate branch. Will attempt to modify by using Action class in selenium to avoid using .click() for fleet

--- src/test/java/vytrack_assignment_one/tc001.java
+++ src/test/java/vytrack_assignment_one/tc001.java
@@ -1,20 +1,74 @@
 package vytrack_assignment_one;
 
 import Utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class tc001 {
 WebDriver driver;
 
+//Acceptance Criteria:
+//1.Verify that truck driver should be able to see all Vehicle information once navigate to Vehicle page.
+//	Given that I am logged into Vytrack
+//	When I click on Fleet Module tab
+//	and click on Vehicle
+//	Then I should be able to view all Vehicle information
+
+    /*
+
+    1. Navigate to vytrack login page: https://qa3.vytrack.com/user/login
+    2. Enter username for truck driver: user4
+    3. Enter password for truck driver: UserUser123
+        3a. Title match - Expected: Dashboard
+    4. Hover over "Fleet"
+    5. Click to "Vehicle"
+    6. Ensure driver has access to all vehicles
+        6a. Title match - Expected: Car - Entities - System - Car - Entities - System
+
+     */
 @BeforeClass
     public void setUp(){
     driver = WebDriverFactory.getDriver("chrome");
     driver.manage().window().maximize();
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     driver.get("https://qa3.vytrack.com/user/login");
+}
+
+@Test
+    public void truckDriverLogIn() throws InterruptedException {
+
+    WebElement usernameField = driver.findElement(By.xpath("//input[@name='_username']"));
+    usernameField.sendKeys("user4");
+    WebElement passwordField = driver.findElement(By.xpath("//input[@name='_password']"));
+    passwordField.sendKeys("UserUser123");
+    WebElement submitButton = driver.findElement(By.xpath("//button[@type='submit']"));
+    submitButton.click();
+
+    Thread.sleep(5000);
+
+    String expTitle = "Dashboard";
+    Assert.assertEquals(driver.getTitle(), expTitle, "Title does not match - Login not successful");
+
+    WebElement fleetModuleTab = driver.findElement(By.xpath("//span[@class='title title-level-1'][1]"));
+    fleetModuleTab.click();
+    fleetModuleTab.click();
+    WebElement vehiclesTab = driver.findElement(By.xpath("//a[@href='entity/Extend_Entity_Carreservation']"));
+    vehiclesTab.click();
+
+    Thread.sleep(5000);
+
+    String expAllVehiclesTitle = "Car - Entities - System - Car - Entities - System";
+    Assert.assertEquals(driver.getTitle(), expAllVehiclesTitle, "Title does not match - Vehicles page not accessed");
+
+
+
+
 
 }
 
