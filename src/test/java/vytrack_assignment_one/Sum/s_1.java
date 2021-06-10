446a21c7a4683b7b9796476ca62410fc436c171f
suleyman <suleyman_yusifov@hotmail.com>
2021-06-09 19:55:01 -0700

    branch s

--- src/test/java/vytrack_assignment_one/Sum/s_1.java
+++ src/test/java/vytrack_assignment_one/Sum/s_1.java
@@ -4,5 +4,8 @@ public class s_1 {
 
     public static void main(String[] args) {
         System.out.println("This is branch work");
+        System.out.println("This is branch2 work");
+        System.out.println("This is branch work");
+        System.out.println("This is3e3 branch work");
     }
 }
