8659c5960f2c8ab3e7c18924eb640c1468fdac52
Zgurskiy11 <zgurskiy11@gmail.com>
2021-07-09 12:08:49 -0600

    new commit

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,8 +2,12 @@ package pages;
 import org.openqa.selenium.support.PageFactory;
 import vytrack_utils.Driver;
 
+import org.openqa.selenium.support.PageFactory;
+import vytrack_utils.Driver;
+
 public class LoginPage {
    public LoginPage(){
        PageFactory.initElements(Driver.getDriver(), this);
+       //some changes
    }
 }
